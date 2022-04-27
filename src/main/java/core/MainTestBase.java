e4d8eb50a2f558d910d48cf6f01a91d9932524af
Михаил <mmamchenkov@fil-it.ru>
2022-04-27 21:08:06 +0300

    актуализация тестов для сайта Gorzdrav

--- src/main/java/core/MainTestBase.java
+++ src/main/java/core/MainTestBase.java
@@ -3,6 +3,7 @@ package core;
 import actions.PageActions;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import listener.RetryRule;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
@@ -54,11 +55,9 @@ public class MainTestBase {
      * Повторный запуск тестов при падении
      */
 
-/*
     @Rule
-    public RetryRule rule = new RetryRule(4);
+    public RetryRule rule = new RetryRule(2);
 
-*/
 
     /**
      * Управление действиями, при различных исходах теста
